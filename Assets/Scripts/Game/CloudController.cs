d3a7f1c89cc6ea696e29282c1daee119a3d68292
isilla <neverovau5@gmail.com>
2024-09-28 22:51:43 +0500

    ASDASDASD

---
+++ Assets/Scripts/Game/CloudController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer.material = new Material(meshRenderer.material);
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            var material = meshRenderer.material;
+            material.mainTextureOffset = new Vector2(material.mainTextureOffset.x+speed*Time.deltaTime, material.mainTextureOffset.y);
+            yield return null;
+        }
+    }
+}
