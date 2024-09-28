4659edf11e8f965bb2d7396b702d5b4a7020b911
isilla <neverovau5@gmail.com>
2024-09-28 16:15:19 +0500

    Add human

--- Assets/Scripts/Game/Human.cs
+++ Assets/Scripts/Game/Human.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 
 public class Human : MonoBehaviour
 {
+    [SerializeField] private List<LineRenderer> lines;
+    [SerializeField] private LineRenderer headLine;
     [SerializeField] private List<Point> points;
 
+    [SerializeField] private Transform ldl;
+    [SerializeField] private Transform rdl;
+    [SerializeField] private Transform ldk;
+    [SerializeField] private Transform rdk;
+    [SerializeField] private Transform p;
+    [SerializeField] private Transform h;
+    [SerializeField] private Transform ls;
+    [SerializeField] private Transform rs;
+    [SerializeField] private Transform lmu;
+    [SerializeField] private Transform rmu;
+    [SerializeField] private Transform lhu;
+    [SerializeField] private Transform rhu;
+    [SerializeField] private int segments = 100;
     public event Action<GameObject> Trigger; 
     
     private void Awake()
@@ -16,7 +31,47 @@ public class Human : MonoBehaviour
             point.Trigger += TriggerHandle;
         }
     }
+
+    private void Update()
+    {
+        lines[0].SetPositions(new []{ ldl.position, ldk.position });
+        lines[1].SetPositions(new []{ rdl.position, rdk.position });
+        lines[2].SetPositions(new []{ ldk.position, p.position });
+        lines[3].SetPositions(new []{ rdk.position, p.position });
+        var s = new Vector3(
+            (ls.position.x + rs.position.x) / 2f, 
+            (ls.position.y + rs.position.y) / 2f+0.1f,
+            (ls.position.z + rs.position.z) / 2f
+            );
+        lines[4].SetPositions(new []{ s, p.position });
+        lines[5].SetPositions(new []{ s, ls.position });
+        lines[6].SetPositions(new []{ s, rs.position });
+        lines[7].SetPositions(new []{ lmu.position, ls.position });
+        lines[8].SetPositions(new []{ rmu.position, rs.position });
+        lines[9].SetPositions(new []{ lmu.position, lhu.position });
+        lines[10].SetPositions(new []{ rmu.position, rhu.position });
+
+        DrawCircle(h.position, Vector3.Distance(s, h.position));
+        //lines[5].SetPositions(new []{ s, h.position });
+    }
     
+    void DrawCircle(Vector3 center, float radius)
+    {
+        headLine.positionCount = segments + 1; // +1 чтобы замкнуть круг
+        headLine.useWorldSpace = true; // Рисуем в мировых координатах
+
+        float angleStep = 360f / segments;
+        for (int i = 0; i <= segments; i++)
+        {
+            float angle = i * angleStep;
+            float x = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
+            float y = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
+
+            Vector3 position = new Vector3(x, y, 0) + center;
+            headLine.SetPosition(i, position);
+        }
+    }
+
     private void TriggerHandle(GameObject obj)
     {
         if (!obj.TryGetComponent<Point>(out _))
