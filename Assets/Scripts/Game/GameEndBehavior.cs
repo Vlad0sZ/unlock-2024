1dc1ed4267e1e8f8f1e285077fe9c3e3ef7fcefe
Vladislav Cherkasov <vlad0s74chel@gmail.com>
2024-09-28 23:15:39 +0500

    Add Game End Behaviour

---
+++ Assets/Scripts/Game/GameEndBehavior.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Invoker;
+using Backend.Registration;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+    public class GameEndBehavior : MonoBehaviour
+    {
+        public void ReloadGame()
+        {
+            SceneManager.LoadScene("MainScene");
+            var invoker = SignalRegistration<ISignalInvoke>.Resolve();
+            invoker.SendCommandToChangeState(0);
+        }
+    }
+}
