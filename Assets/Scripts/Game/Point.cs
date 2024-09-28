fb1a7ffc7784391dc9afa9de2dcc14e46d8d40c0
isilla <neverovau5@gmail.com>
2024-09-28 19:52:55 +0500

    ffff

--- Assets/Scripts/Game/Point.cs
+++ Assets/Scripts/Game/Point.cs
@@ -6,6 +6,7 @@ public class Point : MonoBehaviour
     public event Action<GameObject> Trigger;
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log($"{gameObject.name}    {other.gameObject.name}");
         Trigger?.Invoke(other.gameObject);
     }
 }
