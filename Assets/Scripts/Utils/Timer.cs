1b374f7da2c4e12042e5fa0d520f91606b013bbe
wiftex <wiftexoff@yandex.ru>
2024-09-28 11:48:25 +0500

    Add timer

---
+++ Assets/Scripts/Utils/Timer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils
+{
+    public class Timer : MonoBehaviour
+    {
+        [SerializeField] private int timeInSeconds;
+        [SerializeField] private bool isPause;
+
+        private float _currentTime;
+        
+        public bool IsStarted { get; private set; }
+
+        public event Action<bool> OnPauseChange;
+        public event Action OnStart;
+        public event Action OnStop;
+
+        public void StartTimer()
+        {
+            ResetTimer();
+            IsStarted = true;
+            
+            OnStart?.Invoke();
+        }
+
+        public void SetPauseTimer(bool pause)
+        {
+            isPause = pause;
+            
+            OnPauseChange?.Invoke(isPause);
+        }
+
+        public void TogglePause() => SetPauseTimer(!isPause);
+
+        public void StopTimer()
+        {
+            ResetTimer();
+            IsStarted = false;
+            
+            OnStop?.Invoke();
+        }
+
+        public void ResetTimer()
+        {
+            SetPauseTimer(false);
+            _currentTime = 0;
+        }
+
+        private void Update()
+        {
+            if (!IsStarted) return;
+            
+            _currentTime += Time.deltaTime;
+            if(_currentTime > timeInSeconds)
+                StopTimer();
+        }
+    }
+}
